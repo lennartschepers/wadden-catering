bf90531b3706eecd06709d4e1829d922fa9ec994
Jesse de Boer <jesse@frsc.nl>
2020-05-31 21:20:55 +0200

    Justify between

--- layouts/index.js
+++ layouts/index.js
@@ -10,7 +10,7 @@ const DefaultLayout = ({ children, pages }) => (
   <div>
     <Meta />
     <header className="w-full absolute top-0 left-0 z-50 bg-black text-white bg-opacity-75">
-      <div className="container flex mx-auto py-4 px-4">
+      <div className="container flex mx-auto py-4 px-4 justify-between">
         <div>
           <Link href="/">
             <a>
