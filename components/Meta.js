81e21b3e30519883e5fd353e33abbd1fab9911c1
Jesse de Boer <jesse@frsc.nl>
2020-06-01 12:04:17 +0200

    Fixed json ld

--- components/Meta.js
+++ components/Meta.js
@@ -23,30 +23,26 @@ const Meta = () => {
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: `
-      {
-        "@context": "http://schema.org",
-        "@type": "Restaurant",
-        "address": {
-          "@type": "PostalAddress",
-          "addressLocality": "Anjum",
-          "postalCode": " 9133 DT",
-          "streetAddress": "Oostmahorn 31"
-        },
-        "aggregateRating": {
-          "@type": "AggregateRating",
-          "ratingValue": "4.5",
-          "reviewCount": "307"
-        },
-        "name": "Het Raadsel van de Wadden",
-        "openingHours": [
-          "Mo-Fr 17:00-22:00",
-          "Sa-Su 12:00-16:00",
-          "Sa-Su 17:00-22:00",
-        ],
-        "acceptsReservations": true,
-        "telephone": "06 12069380",
-        "url": "https://restaurantlauwersmeer.nl"
-      }`,
+            {
+              "@context": "http://schema.org",
+              "@type": "Restaurant",
+              "image": "https://images.prismic.io/raadselvandewadden/ad3a96fd-9f0d-498f-b4fe-3d69a8bc8d6b_IMG_4778-kopie.jpg?auto=compress,format&amp;rect=0,313,2048,910&amp;w=1800&amp;h=800",
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Anjum",
+                "postalCode": " 9133 DT",
+                "streetAddress": "Oostmahorn 31"
+              },
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.5",
+                "reviewCount": "307"
+              },
+              "name": "Het Raadsel van de Wadden",
+              "acceptsReservations": true,
+              "telephone": "06 12069380",
+              "url": "https://restaurantlauwersmeer.nl"
+            }`,
           }}
         />
       </Head>
