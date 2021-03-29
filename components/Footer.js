6e0cab23d1a38a8208435b332a8f0c5e0c7d136b
Jesse de Boer <jesse@frsc.nl>
2021-03-29 23:08:18 +0200

    Load instantly formitable

--- components/Footer.js
+++ components/Footer.js
@@ -53,29 +53,18 @@ const Footer = ({ footer }) => {
             </div>
           </div>
         </div>
-
         <script
           dangerouslySetInnerHTML={{
-            __html: `setTimeout(() => {(function (d, s, id, h) {
-          var ftjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          var js = d.createElement(s);
-          js.id = id;
-          js.src = "https://cdn.formitable.com/sdk/v1/ft.sdk.min.js";
-          h && (js.onload = h);
-          ftjs.parentNode.insertBefore(js, ftjs);
-        })(document, "script", "formitable-sdk", function () {
-          FT.load("Analytics");
-        })},1000)`,
+            __html: `(function (d, s, id, h) { var ftjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; var js = d.createElement(s); js.id = id; js.src = "https://cdn.formitable.com/sdk/v1/ft.sdk.min.js"; h && (js.onload = h); ftjs.parentNode.insertBefore(js, ftjs); }(document, 'script', 'formitable-sdk', function () { FT.load('Analytics'); }));`,
           }}
         />
         <div
-          className="ft-widget-b2"
+          class="ft-widget-b2"
           data-restaurant="618e6e7e"
-          data-open="false"
+          data-open="1500"
           data-open-mobile="false"
           data-color="#C5AB6B"
-          data-language="nl"
+          data-language="auto"
           data-tag="Website"
           data-toolbar="true"
           data-toolbar-mobile="true"
